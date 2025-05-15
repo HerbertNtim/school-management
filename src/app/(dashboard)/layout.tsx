096f8ceb7909a3c2283efebbd64fc8b8de4a9018
HerbertNtim <herbertntim15@gmail.com>
2025-05-15 08:49:59 +0000

    developed side bar

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,12 +1,21 @@
+import Menu from "@/components/Menu"
+import Image from "next/image"
+import Link from "next/link"
 
 const DashboardLayout = () => {
   return (
-    <main className="h-screen flex">
+    <main className="h-full flex">
       {/* LEFT */}
-      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-red-200">l</div>
+      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4">
+        <Link href='/' className="flex items-center justify-center lg:justify-start gap-2">
+          <Image src='/logo.png' alt={'logo'} width={34} height={34} />
+          <span className="hidden lg:block text-2xl text-gray-600">School</span>
+        </Link>
+        <Menu />
+      </div>
 
       {/* RIGHT */}
-      <div className="bg-blue-200 w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%]">r</div>
+      <div className="bg-[#F7F8FA] w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] overflow-scroll">r</div>
     </main>
   )
 }
