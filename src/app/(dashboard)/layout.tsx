9965c249cab964f548318f1bbfdbc0d499d900fd
HerbertNtim <herbertntim15@gmail.com>
2025-05-15 08:07:17 +0000

    added dashboard

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,7 +1,13 @@
 
-const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+const DashboardLayout = () => {
   return (
-    <main className="h-screen flex">{children}</main>
+    <main className="h-screen flex">
+      {/* LEFT */}
+      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-red-200">l</div>
+
+      {/* RIGHT */}
+      <div className="bg-blue-200 w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%]">r</div>
+    </main>
   )
 }
 
