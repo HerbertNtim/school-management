171c4b1bbef081e08f3845499877cb4942e738e8
HerbertNtim <herbertntim15@gmail.com>
2025-05-15 07:40:01 +0000

    adding role based routes

---
+++ src/app/(dashboard)/layout.tsx
@@ -0,0 +1,8 @@
+
+const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <main className="h-screen flex">{children}</main>
+  )
+}
+
+export default DashboardLayout
