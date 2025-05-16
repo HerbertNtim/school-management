0f435ca238b03c5a6c1af7acc0220b0af83de608
HerbertNtim <herbertntim15@gmail.com>
2025-05-16 08:17:52 +0000

    setup env for recharts

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -5,12 +5,25 @@ const AdminPage = () => {
   return (
     <div className='flex flex-col md:flex-row gap-4 p-4'>
       {/* LEFT */}
-      <div className='w-full lg:w-2/3 flex flex-wrap gap-8'>
-        <UserCard type='student' />
-        <UserCard type='teacher' />
-        <UserCard type='parent' />
-        <UserCard type='staff' />
+      <div className='w-full lg:w-2/3'>
+        {/* USER CARDS */}
+        <div className='flex flex-wrap gap-4 justify-between'>
+          <UserCard type='student' />
+          <UserCard type='teacher' />
+          <UserCard type='parent' />
+          <UserCard type='staff' />
+        </div>
+        {/* CENTER CHARTS */}
+        <div>
+          {/* COUNT CHART */}
+          <div></div>
+          {/* ATTENDANCE CHART */}
+          <div></div>
+        </div>
+        {/* BOTTOM CHARTS */}
+        <div></div>
       </div>
+
       {/* RIGHT */}
       <div className='w-full lg:w-1/3'>
         r
