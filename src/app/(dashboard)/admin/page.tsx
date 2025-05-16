081f5e28978a946122d3cdbba93475514ef74a82
HerbertNtim <herbertntim15@gmail.com>
2025-05-16 10:30:11 +0000

    dev: developed count chart

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,3 +1,6 @@
+'use client'
+
+import CountChart from '@/components/CountChart'
 import UserCard from '@/components/UserCard'
 import React from 'react'
 
@@ -5,7 +8,7 @@ const AdminPage = () => {
   return (
     <div className='flex flex-col md:flex-row gap-4 p-4'>
       {/* LEFT */}
-      <div className='w-full lg:w-2/3'>
+      <div className='w-full lg:w-2/3 flex flex-col gap-8'>
         {/* USER CARDS */}
         <div className='flex flex-wrap gap-4 justify-between'>
           <UserCard type='student' />
@@ -14,11 +17,13 @@ const AdminPage = () => {
           <UserCard type='staff' />
         </div>
         {/* CENTER CHARTS */}
-        <div>
+        <div className='flex gap-4 lg:flex-row flex-col mt-4'>
           {/* COUNT CHART */}
-          <div></div>
+          <div className='w-full lg:w-1/3 h-[450px]'>
+            <CountChart />
+          </div>
           {/* ATTENDANCE CHART */}
-          <div></div>
+          <div className='w-full lg:w-2/3 h-[450px]'></div>
         </div>
         {/* BOTTOM CHARTS */}
         <div></div>
