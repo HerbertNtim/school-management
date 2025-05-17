c676449a68d8f37e998bbba7a49b3e8e24fd12df
HerbertNtim <herbertntim15@gmail.com>
2025-05-17 15:49:28 +0000

    dev: calendar

--- src/components/EventCalendar.tsx
+++ src/components/EventCalendar.tsx
@@ -1,15 +1,39 @@
+import { title } from 'process';
 import { useState } from 'react';
 import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
 
 type ValuePiece = Date | null;
 
 type Value = ValuePiece | [ValuePiece, ValuePiece];
 
+// Temporary
+const events = [
+  {
+    id: 1,
+    title: 'Event 1',
+    time: "12:00 PM -  2:00 PM",
+    description: "This is a description of event 1", 
+  },
+  {
+    id: 2,
+    title: 'Event 2',
+    time: "3:00 PM - 4:00 PM",
+    description: "This is a description of event 2", 
+  },
+  {
+    id: 3,
+    title: 'Event 3',
+    time: "5:00 PM - 6:00 PM",
+    description: "This is a description of event 3", 
+  },
+]
+
 const EventCalendar = () => {
   const [value, onChange] = useState<Value>(new Date());
 
   return (
-    <div><Calendar onChange={onChange} value={value} /></div>
+    <div className='bg-white p-4 rounded-md'><Calendar onChange={onChange} value={value} /></div>
   )
 }
 
