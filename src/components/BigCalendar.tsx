acafa8223452618746400d2e65510c7b0328c821
HerbertNtim <herbertntim15@gmail.com>
2025-05-19 12:46:13 +0000

    dev: big calender added

--- src/components/BigCalendar.tsx
+++ src/components/BigCalendar.tsx
@@ -1,20 +1,33 @@
-import { Calendar, momentLocalizer } from 'react-big-calendar'
+'use client'
+
+import { Calendar, momentLocalizer, View, Views } from 'react-big-calendar'
+import 'react-big-calendar/lib/css/react-big-calendar.css'
 import moment from 'moment'
 import { calendarEvents } from '@/lib/data'
+import { useState } from 'react'
 
 const localizer = momentLocalizer(moment)
 
-const BigCalendar = () => (
-  <div>
+const BigCalendar = () => {
+  const [view, setView] = useState<View>(Views.WORK_WEEK)
+  const handleViewChange = (selectedView: View) => {
+    setView(selectedView)
+  }
+
+  return (
     <Calendar
       localizer={localizer}
       events={calendarEvents}
       startAccessor="start"
       endAccessor="end"
-      style={{ height: 500 }}
+      views={["work_week", "day"]}
+      view={view}
+      style={{ height: '98%' }}
+      onView={handleViewChange}
+      min={new Date(0, 0, 0, 8, 0, 0)}
+      max={new Date(0, 0, 0, 17, 0, 0)}
     />
-  </div>
-) 
+  )
+}
 
 export default BigCalendar
- 
