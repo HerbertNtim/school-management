16cda2ec1f33a94a8d9d1d04ba7b3ee4bfaa8479
HerbertNtim <herbertntim15@gmail.com>
2025-05-20 10:18:50 +0000

    dev: developed the top part of the list

--- src/components/TableSearch.tsx
+++ src/components/TableSearch.tsx
@@ -1,8 +1,12 @@
+import Image from 'next/image'
 import React from 'react'
 
 const TableSearch = () => {
   return (
-    <div>TableSearch</div>
+    <div className='w-full md:w-auto flex items-center gap-2 rounded-full text-xs border ring-[1.5px] ring-gray-300 px-2'>
+      <Image src='/search.png' alt='search' width={15} height={15} />
+      <input type="text" placeholder='Search...' className='w-[200px] p-2 bg-transparent outline-none' />
+    </div>
   )
 }
 
