48438f3ccca346a4d67a48b35d1fe6d764f2c8fe
HerbertNtim <herbertntim15@gmail.com>
2025-05-28 08:05:48 +0000

    starting teachers single page

--- src/app/(dashboard)/list/announcements/page.tsx
+++ src/app/(dashboard)/list/announcements/page.tsx
@@ -1,18 +1,16 @@
 import Pagination from '@/components/Pagination'
 import Table from '@/components/Table'
 import TableSearch from '@/components/TableSearch'
-import { eventsData, role } from '@/lib/data'
+import { announcementsData, role } from '@/lib/data'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
-type Event = {
+type Announcement = {
   id: number;
   title: string;
   class: string;
   date: string;
-  startTime: string;
-  endTime: string;
 }
 
 const buttonList = [
@@ -30,7 +28,7 @@ const buttonList = [
   }
 ]
 
-const eventsColumns = [
+const announcementColumns = [
   {
     header: 'Title',
     accessor: 'titles',
@@ -46,35 +44,23 @@ const eventsColumns = [
     className: 'hidden md:table-cell'
   },
   {
-    header: 'Start Time',
-    accessor: 'start',
-    className: 'hidden md:table-cell'
-  },
-  {
-    header: 'End Time',
-    accessor: 'end',
-    className: 'hidden md:table-cell'
-  },
-  {
     header: 'Actions',
     accessor: 'actions',
   }
 ]
 
-const renderRow = (event: Event) => (
-  <tr key={event.id} className='border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-customPurpleLight'>
+const renderRow = (announcement: Announcement) => (
+  <tr key={announcement.id} className='border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-customPurpleLight'>
     <td className='flex items-center gap-4 p-4'>
       <div className='flex flex-col items-center gap-2'>
-        <h3 className='font-semibold'>{event.title}</h3>
+        <h3 className='font-semibold'>{announcement.title}</h3>
       </div>
     </td>
-    <td className='hidden md:table-cell'>{event.class}</td>
-    <td className='hidden md:table-cell'>{event.date}</td>
-    <td className='hidden md:table-cell'>{event.startTime}</td>
-    <td className='hidden md:table-cell'>{event.endTime}</td>
+    <td className='hidden md:table-cell'>{announcement.class}</td>
+    <td className='hidden md:table-cell'>{announcement.date}</td>
     <td>
       <div className='flex items-center gap-2'>
-        <Link href={`/list/teachers/${event.id}`}>
+        <Link href={`/list/teachers/${announcement.id}`}>
           <button className='w-8 h-8 flex items-center justify-center rounded-full bg-sky cursor-pointer'>
             <Image src='/view.png' alt='Edit' width={16} height={16} />
           </button>
@@ -89,12 +75,12 @@ const renderRow = (event: Event) => (
   </tr>
 )
 
-const EventsList = () => {
+const AnnouncementsPage = () => {
   return (
     <div className='bg-white p-4 rounded-md flex-1 m-4 mt-0'>
       {/*  TOP */}
       <div className='flex items-center justify-between'>
-        <h1 className='hidden md:block text-lg font-semibold'>All Events</h1>
+        <h1 className='hidden md:block text-lg font-semibold'>All Announcements</h1>
         <div className='flex flex-col md:flex-row items-center gap-4 w-full md:w-auto'>
           <TableSearch />
           <div className='flex items-center gap-4 self-end'>
@@ -107,11 +93,11 @@ const EventsList = () => {
         </div>
       </div>
       {/* LIST */}
-      <Table columns={eventsColumns} renderRow={renderRow} data={eventsData} />
+      <Table columns={announcementColumns} renderRow={renderRow} data={announcementsData} />
       {/* PAGINATION */}
       <Pagination />
     </div>
   )
 }
 
-export default EventsList
+export default AnnouncementsPage
---
+++ src/app/(dashboard)/list/teachers/[id]/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const SingleTeacherPage = () => {
+  return (
+    <div className='flex-1 p-4 flex flex-col gap-4 xl:flex-row'>
+      {/* LEFT */}
+      <div className='w-full xl:w-2/3'>
+        {/* INFO CARD */}
+        <div></div>
+      </div>
+      {/* RIGHT */}
+      <div className='w-full xl:w-1/3'>r</div>
+    </div>
+  )
+}
+
+export default SingleTeacherPage
